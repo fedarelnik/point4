7cbce3f9d4ee7b0f01045b7ce821c2febac57ca5
fedarelnik <fedarelnik@gmail.com>
2020-03-09 23:50:50 +0300

    error glibc stdio

--- init.h
+++ init.h
@@ -3,58 +3,14 @@
 #include <math.h>
 #include <string.h>
 
-void area_lines(float bound[][4], float coord[][2], int n){
-
-}
-void minX(float point[2], float coord[][2], int n){
-point[0] = coord[0][0];
-point[1] = coord[0][1];
-for(int i=1;i<n;i++){
-if(coord[i][0]<point[0]){
-point[0] = coord[i][0];
-point[1] = coord[i][1];
-}
-}
-}
-void maxX(float point[2], float coord[][2], int n){
-point[0] = coord[0][0];
-point[1] = coord[0][1];
-for(int i=1;i<n;i++){
-if(coord[i][0]>point[0]){
-point[0] = coord[i][0];
-point[1] = coord[i][1];
-}
-}
-}
-void minY(float point[2], float coord[][2], int n){
-point[0] = coord[0][0];
-point[1] = coord[0][1];
-for(int i=1;i<n;i++){
-if(coord[i][1]<point[1]){
-point[0] = coord[i][0];
-point[1] = coord[i][1];
-}
-}
-}
-void maxY(float point[2], float coord[][2], int n){
-point[0] = coord[0][0];
-point[1] = coord[0][1];
-for(int i=1;i<n;i++){
-if(coord[i][1]>point[1]){
-point[0] = coord[i][0];
-point[1] = coord[i][1];
-}
-}
-}
-
 float dist(float x1, float y1, float x2, float y2){
 return sqrt(pow(x1-x2,2)+pow(y1-y2,2));
 }
 
 void open_coord(float coord[][2], int n, char filename[]){
 int i = 0, j = 0;
-//printf("filename is: %s\n", filename);
-//printf("number: %d\n", n);
+printf("filename is: %s\n", filename);
+printf("number: %d\n", n);
 FILE *fp;
 char c, num[10];
 memset(num, '\0', 10);
@@ -63,14 +19,17 @@ do{
 c = getc(fp);
 switch(c){
 case ' ':
-coord[i][0]=atof(num);
-//printf("x%d: %f\n", i, atof(num));
+printf("here  \n");
+//coord[i][0]=atof(num);
+printf("before");
+printf("x%d: %s\n", i, num);
 j = 0;
 memset(num, '\0', 10);
 break;
 case '\n':
-coord[i][1]=atof(num);
-//printf("y%d: %f\n", i, atof(num));
+printf("here n\n");
+//coord[i][1]=atof(num);
+printf("y%d: %s\n", i, num);
 j = 0;
 i++;
 memset(num, '\0', 10);
--- point4.c
+++ point4.c
@@ -5,21 +5,12 @@
 #include "init.h"
 
 int main(int argc, char *argv[]){
-int n = 4;
-char coordfile[] = "coord.txt";
+int n = 50;
+char coordfile[] = "koordinaty.txt";
 float coord_array[n][2];
 open_coord(coord_array, n, coordfile);
-for(int i=0;i<n;i++){
-printf("%d) %f | %f\n", i+1, coord_array[i][0], coord_array[i][1]);
-}
-float minXpoint[2], maxXpoint[2], minYpoint[2], maxYpoint[2];
-minX(minXpoint, coord_array, n);
-printf("minXpoint = %f | %f\n", minXpoint[0], minXpoint[1]);
-maxX(maxXpoint, coord_array, n);
-printf("maxXpoint = %f | %f\n", maxXpoint[0], maxXpoint[1]);
-minY(minYpoint, coord_array, n);
-printf("minYpoint = %f | %f\n", minYpoint[0], minYpoint[1]);
-maxY(maxYpoint, coord_array, n);
-printf("maxYpoint = %f | %f\n", maxYpoint[0], maxYpoint[1]);
+//for(int i=0;i<n;i++){
+//printf("%d) %f | %f\n", i+1, coord_array[i][0], coord_array[i][1]);
+//}
 return 0;
 }
